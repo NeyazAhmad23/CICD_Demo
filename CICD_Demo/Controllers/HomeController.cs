bf335068bd6cc25eb30a470b106d64417e6dcb60
Neyaz Ahmad <A134996@agl.com.au>
2017-10-13 18:53:27 +1100

    Test

--- CICD_Demo/Controllers/HomeController.cs
+++ CICD_Demo/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace CICD_Demo.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page...";
+            ViewBag.Message = "Your contact page...!!";
 
             return View();
         }
